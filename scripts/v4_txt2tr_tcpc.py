29b89a8e25fb13b0a28f9dbc5a1c956986415e18
voidf <510030805@qq.com>
2025-10-07 00:15:46 +0800

    avoid networking block translation

--- scripts/v4_txt2tr_tcpc.py
+++ scripts/v4_txt2tr_tcpc.py
@@ -1,21 +1,15 @@
-import functools
 import os
-os.environ["ARGOS_DEVICE_TYPE"] = "cuda"
+# os.environ["ARGOS_DEVICE_TYPE"] = "cuda"
 import asyncio
-import hashlib
 import traceback
-
-import msgpack
-import hmac
-import struct
 import time
 import gc
+import multiprocessing as mp
 from datetime import datetime
 from typing import List
 
 import argostranslate.package as ARGOSPKG
 import ctranslate2
-import zstandard as zstd
 from loguru import logger
 
 from v4_helpers import unload_unused_cache, rpc, get_or_install_package, get_translator, \
@@ -25,6 +19,7 @@ from v4_helpers import unload_unused_cache, rpc, get_or_install_package, get_tra
 # Config
 # -----------------------------
 
+TQUEUE_SIZE = 8
 MAX_TOKENS_PER_BATCH = 1024
 DEVICE = os.environ.get("ARGOS_DEVICE_TYPE", "cpu")
 
@@ -75,35 +70,44 @@ def translate_fast(
 # -----------------------------
 # 主循环
 # -----------------------------
-async def tcp_main():
+
+def proc_get_task(q: mp.Queue):
+    async def f():
+        while True:
+            # 取任务
+            try:
+                task = await rpc("g", {"v": 1})
+            except Exception as e:
+                logger.error(f"[client] fetch error: {traceback.format_exc()} SLEEP 5s")
+                time.sleep(5); continue
+            task_o = task.get("o", -1)
+            if task_o != 0:
+                logger.warning(f"task o == {task_o}, SLEEP 5s")
+                time.sleep(5); continue
+            src = task["s"]; dst = task["t"]; data = task["p"]
+            logger.info(f"{datetime.now()} recv task: {src}>{dst} sentences:{len(data)}")
+            q.put((src, dst, data))
+    asyncio.run(f())
+
+def proc_submit_task(q: mp.Queue):
+    async def f():
+        while 1:
+            try:
+                s, t, p = q.get()
+                resp = await rpc("u", {"s": s, "t": t, "p": p})
+                logger.info(f"submit done. resp: {resp}")
+            except Exception as e:
+                logger.error(f"[client] upload error: {e}")
+    asyncio.run(f())
+
+def proc_tr(in_q: mp.Queue, out_q: mp.Queue):
     logger.info(f"[client] API={API_HOST}:{API_PORT}  MAX_TOKENS_PER_BATCH={MAX_TOKENS_PER_BATCH}  DEVICE={DEVICE}")
-    while True:
-        # 取任务
-        try:
-            task = await rpc("g", {"v": 1})
-            # r = requests.get(f"{API}/", headers=ALLOW_COMPRESS, timeout=REQUEST_TIMEOUT)
-            # r.raise_for_status()
-            # task = r.json()
-        except Exception as e:
-            logger.error(f"[client] fetch error: {traceback.format_exc()} SLEEP 5s")
-            time.sleep(5); continue
-        task_o = task.get("o", -1)
-        if task_o != 0:
-            logger.warning(f"task o == {task_o}, SLEEP 5s")
-            time.sleep(5); continue
-
-        src = task["s"]; dst = task["t"]; data = task["p"]
-        logger.info(f"{datetime.now()} recv task: {src}>{dst} sentences:{len(data)}")
-
-        # 包 + 引擎 + SBD
+    while 1:
+        src, dst, data = in_q.get()
         pkg = get_or_install_package(src, dst)
         translator = get_translator(src, dst, pkg)
-
-        # 对于内存不足的机器，需要把用不到的模型卸载
         if os.environ.get("MSAVE", ""):
             unload_unused_cache(src, dst)
-
-        # 翻译
         t0 = datetime.now()
         try:
             outs, token_count = translate_fast(
@@ -116,15 +120,16 @@ async def tcp_main():
         t1 = datetime.now()
         secs = (t1 - t0).total_seconds()
         logger.info(f"[client] {t1} batch {len(data)} paras {token_count} tokens {secs:.3f}s  ~{token_count/max(1e-12,secs):.4f}tk/s")
-        
-        try:
-            resp = await rpc("u", {"s": src, "t": dst, "p": list(zip(data, outs))})
-            logger.info(f"submit done. resp: {resp}")
-            # rr = requests.post(f"{API}/u", json=payload, headers=ALLOW_COMPRESS, timeout=REQUEST_TIMEOUT)
-            # rr.raise_for_status()
-        except Exception as e:
-            logger.error(f"[client] upload error: {e}")
-            # time.sleep(3)
-        gc.collect()
+        out_q.put((src, dst, list(zip(data, outs))))
+
 if __name__ == "__main__":
-    asyncio.run(tcp_main())
+    mgr = mp.Manager()
+    q1 = mgr.Queue(maxsize=TQUEUE_SIZE)
+    q2 = mgr.Queue()
+    procs = [
+        mp.Process(target=proc_get_task, args=(q1,)),
+        mp.Process(target=proc_submit_task, args=(q2,)),
+        mp.Process(target=proc_tr, args=(q1,q2,))
+    ]
+    for x in procs: x.start()
+    for x in procs: x.join()
